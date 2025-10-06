3d55665d787790959988d695af629401a613e1dd
medvedevaviktoria <medvedevaviktoria176@gmail.com>
2025-10-06 16:44:54 +0300

    Добавление основных файлов проекта

---
+++ Arkanoid/Arkanoid/Classes/Ball.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arkanoid.Classes
+{
+    internal class Ball
+    {
+        public Rectangle Rect { get; set; }
+        public Point Position { get; set; }
+
+        public Ball(Rectangle rect, Point position)
+        {
+            Rect = rect;
+            Position = position;
+        }
+    }
+}
